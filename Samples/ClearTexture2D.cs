3204405dc7d6341df07fad33b4c82737efc56d53
Narazaka <info@narazaka.net>
2023-11-15 08:40:26 +0900

    samples

---
+++ Samples/ClearTexture2D.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace net.narazaka.vrchat.sync_texture.samples
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ClearTexture2D : UdonSharpBehaviour
+    {
+        [SerializeField]
+        Texture2D[] Textures;
+
+        public void ClearTexture()
+        {
+            foreach (var texture in Textures)
+            {
+                var colors = new Color[texture.width * texture.height];
+                texture.SetPixels(colors);
+                texture.Apply();
+            }
+        }
+    }
+}
---
+++ Samples/InteractSend.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace net.narazaka.vrchat.sync_texture.samples
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class InteractSend : UdonSharpBehaviour
+    {
+        [SerializeField]
+        UdonSharpBehaviour Target;
+        [SerializeField]
+        string EventName;
+
+        public override void Interact()
+        {
+            Target.SendCustomEvent(EventName);
+        }
+    }
+}
