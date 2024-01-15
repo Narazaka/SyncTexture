7083bf5b2f22e87cacf3cefe9b00c77d4f52ae06
Narazaka <info@narazaka.net>
2024-01-15 17:03:36 +0900

    doc

--- SyncTextureBase.cs
+++ SyncTextureBase.cs
@@ -10,13 +10,43 @@ namespace net.narazaka.vrchat.sync_texture
 {
     public abstract class SyncTextureBase : UdonSharpBehaviour
     {
+        /// <summary>
+        /// If false, do nothing.
+        /// </summary>
+        [PublicAPI]
         [SerializeField]
         public bool SyncEnabled = true;
         [SerializeField]
         public UdonBehaviour CallbackListener;
 
+        /// <summary>
+        /// Take ownership and send texture data to other players.
+        /// 
+        /// If not SyncEnabled, do nothing.
+        /// </summary>
+        /// <returns>
+        /// actually started or not
+        /// </returns>
+        [PublicAPI]
         public abstract bool StartSync();
+        /// <summary>
+        /// Take ownership and stop sending.
+        /// </summary>
+        /// <returns>
+        /// actually canceled or not
+        /// </returns>
+        [PublicAPI]
         public abstract bool CancelSync();
+        /// <summary>
+        /// Take ownership and force start sending.
+        /// 
+        /// If already sending, abort and restart sending.
+        /// If not SyncEnabled, do nothing.
+        /// </summary>
+        /// <returns>
+        /// actually started or not
+        /// </returns>
+        [PublicAPI]
         public abstract bool ForceStartSync();
     }
 }
