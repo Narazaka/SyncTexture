2b72f676a9c7537be5f0137454089812790d4260
Narazaka <info@narazaka.net>
2024-12-06 06:29:35 +0900

    receive log

--- SyncTexture2D8.cs
+++ SyncTexture2D8.cs
@@ -41,6 +41,7 @@ namespace net.narazaka.vrchat.sync_texture
 
         public void ApplyReceiveColorsPartial(SyncTextureData8 data)
         {
+            Debug.Log($"[SyncTexture] ApplyReceiveColorsPartial() [ReceiveEnabled={ReceiveEnabled}]");
             Callback(nameof(SyncTextureCallbackListener.OnReceive));
             if (!ReceiveEnabled) return;
             var syncIndex = Array.IndexOf(DataList, data);
